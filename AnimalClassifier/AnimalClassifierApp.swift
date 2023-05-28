3e5ce0b0329d0a2ff13d8de1f9130853c7248e60
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-05-28 12:44:38 +0600

    project init

---
+++ AnimalClassifier/AnimalClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalClassifierApp.swift
+//  AnimalClassifier
+//
+//  Created by Shah Md Imran Hossain on 28/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimalClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
