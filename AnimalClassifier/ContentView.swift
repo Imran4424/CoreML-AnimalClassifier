dc5463baec1b70c04987009b146c5e8a329a90eb
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-05-28 18:28:20 +0600

    implement imagePicker

--- AnimalClassifier/ContentView.swift
+++ AnimalClassifier/ContentView.swift
@@ -5,17 +5,70 @@
 //  Created by Shah Md Imran Hossain on 28/5/23.
 //
 
+import CoreML
+import PhotosUI
 import SwiftUI
+import Vision
 
 struct ContentView: View {
+    @State private var isShowingImagePicker = false
+    @State private var imageItem: PhotosPickerItem?
+    @State private var selectedImage: Image?
+    @State private var predictedAnimal: String = "None"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                ScrollView(.horizontal) {
+                    HStack {
+                        Image("dog")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(maxWidth: .infinity)
+                        Image("cat")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(maxWidth: .infinity)
+                        Image("rabbit")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(maxWidth: .infinity)
+                    }
+                }
+                .frame(maxHeight: .infinity)
+                
+                VStack {
+                    if let selectedImage {
+                        selectedImage
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 300, height: 300)
+                    }
+                    
+                    PhotosPicker("Select an Image to classify", selection: $imageItem, matching: .images)
+                }
+                .frame(maxHeight: .infinity)
+            }
+            .onChange(of: imageItem) { _ in
+                Task {
+                    if let data = try? await imageItem?.loadTransferable(type: Data.self) {
+                        if let uiImage = UIImage(data: data) {
+                            selectedImage = Image(uiImage: uiImage)
+                            return
+                        }
+                    }
+                    
+                    print("Failed")
+                }
+            }
         }
-        .padding()
+    }
+}
+
+// MARK: - Actions
+extension ContentView {
+    func recognizeAnimal() {
+        
     }
 }
 
